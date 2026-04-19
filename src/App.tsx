acc83af8bb5e4618d9d9fd5702fb7ba1db54de2d
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-19 21:15:18 +0300

    define styles for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 function App() {
   return (
     <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nemo
-      optio doloribus pariatur praesentium molestias blanditiis iure cum. Enim,
-      laborum possimus. Neque modi voluptate, doloribus iusto recusandae quos
-      accusamus in mollitia, quasi asperiores libero, impedit voluptatem maiores
-      ullam dignissimos aperiam ipsam voluptatibus quis facere est. Repellendus
-      corporis nulla ipsam natus!
+      <button className="btn btn--tip">5%</button>
     </>
   );
 }
