8accd287f1259ffedc2793eb233d4ee307fbb7d4
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-19 20:55:37 +0300

    define base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia nemo
+      optio doloribus pariatur praesentium molestias blanditiis iure cum. Enim,
+      laborum possimus. Neque modi voluptate, doloribus iusto recusandae quos
+      accusamus in mollitia, quasi asperiores libero, impedit voluptatem maiores
+      ullam dignissimos aperiam ipsam voluptatibus quis facere est. Repellendus
+      corporis nulla ipsam natus!
+    </>
+  );
 }
 
 export default App;
