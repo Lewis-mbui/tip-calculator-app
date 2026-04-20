215957fefb5649175cc48f9c151caa92e1816d07
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-20 08:25:58 +0300

    build structure of FormField component

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,5 +1,31 @@
-const FormField = () => {
-  return <div>FormField</div>;
+interface Props {
+  id: string;
+  label: string;
+  icon: string;
+  error: string | undefined | null;
+  isDisabled: boolean;
+}
+
+const FormField = ({ label, icon, id, isDisabled, error }: Props) => {
+  const errorClass = error ? "error" : "";
+
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      {error && <p className="error">{error}</p>}
+      <div className={`input-group ${errorClass}`}>
+        <img src={icon} alt="" />
+        <input
+          disabled={isDisabled}
+          id={id}
+          placeholder="0"
+          type="number"
+          name={id}
+          className="primary-input"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FormField;
--- src/components/TipForm.tsx
+++ src/components/TipForm.tsx
@@ -1,12 +1,27 @@
+import dollarIcon from "../assets/images/icon-dollar.svg";
+import personIcon from "../assets/images/icon-person.svg";
+
 import FormField from "./FormField";
 import TipPercentageField from "./TipPercentageField";
 
 const TipForm = () => {
   return (
     <form className="tip-form" action="">
-      <FormField />
+      <FormField
+        id="bill"
+        label="Bill"
+        icon={dollarIcon}
+        isDisabled={false}
+        error={null}
+      />
       <TipPercentageField />
-      <FormField />
+      <FormField
+        id="totalPeople"
+        label="Number of People"
+        icon={personIcon}
+        isDisabled={true}
+        error={null}
+      />
     </form>
   );
 };
