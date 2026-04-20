fd42d9a7461edbb3a37203c0bd431a670d530d34
Lewis-mbui <lewmugo14479@gmail.com>
2026-04-20 08:31:29 +0300

    structure for TipPercentageField component

--- src/components/TipPercentageField.tsx
+++ src/components/TipPercentageField.tsx
@@ -1,5 +1,25 @@
-const TipPercentageField = () => {
-  return <div>TipPercentageField</div>;
+interface Props {
+  isDisabled: boolean;
+}
+
+const TipPercentageField = ({ isDisabled }: Props) => {
+  return (
+    <div className="form-group percentage">
+      <label htmlFor="custom-val">Select Tip %</label>
+      <button className="btn btn--block btn--tip">5%</button>
+      <button className="btn btn--block btn--tip">10%</button>
+      <button className="btn btn--block btn--tip">15%</button>
+      <button className="btn btn--block btn--tip">25%</button>
+      <button className="btn btn--block btn--tip">50%</button>
+      <input
+        disabled={isDisabled}
+        id="custom-val"
+        placeholder="Custom"
+        type="number"
+        className="custom-input"
+      />
+    </div>
+  );
 };
 
 export default TipPercentageField;
